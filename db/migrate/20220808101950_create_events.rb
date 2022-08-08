95e57e917d2f3f9cdb2aec346368699559fe7217
Pierre-François Salmand <maabaa@gmail.com>
2022-08-08 20:44:57 +0700

    Make Events table work

--- db/migrate/20220808101950_create_events.rb
+++ db/migrate/20220808101950_create_events.rb
@@ -1,12 +1,13 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
+      t.belongs_to :admin, index: true
+      t.string :title
+      t.string :location
+      t.text :description
       t.datetime :start_date
       t.integer :duration
-      t.string :title
-      t.text :description
       t.integer :price
-      t.string :location
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'table_print'
+
+Faker::Config.locale = 'fr'
+
+until User.count == 50
+  User.create(
+    email: Faker::Internet.email,
+    encrypted_password: Faker::Internet.password,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    description: Faker::Quote.yoda
+  )
+end
+users = User.all
+
+puts "\n"
+puts '~' * 50
+puts "\n"
+print "SAMPLE USER: \n\n"
+tp users, except: %i[created_at updated_at]
+
+until Event.count == 20
+  Event.create!(
+    admin: users.sample,
+    start_date: Faker::Date.forward(days: 30),
+    title: Faker::Book.title,
+    location: Faker::Address.city,
+    duration: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60].sample,
+    description: Faker::Quote.yoda,
+    price: rand(10..200)
+  )
+end
+events = Event.all
+
+puts "\n"
+puts '~' * 50
+puts "\n"
+print "SAMPLE EVENT: \n\n"
+tp events, except: %i[created_at updated_at]
+
+puts "\n"
+puts '~' * 50
+puts "\n"
