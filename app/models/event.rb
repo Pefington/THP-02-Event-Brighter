d4c77933bd9f59101bc43d2c62876920d3011c70
Pierre-François Salmand <maabaa@gmail.com>
2022-08-08 23:42:30 +0700

    Write Event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,37 @@
 class Event < ApplicationRecord
   after_create :confirm_send
-  # validates :start_date, presence: true
-  # validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :start_date,
+            presence: true,
+            if: :start_date_cannot_be_in_the_past
+  validates :duration,
+            presence: true,
+            numericality: { only_integer: true, greater_than: 0 },
+            if: :not_multiple_of_five?
+  validates :title,
+            presence: true,
+            length: { in: 5..140 }
+  validates :description,
+            presence: true,
+            length: { in: 20..1000 }
+  validates :price,
+            presence: true,
+            numericality: { only_integer: true, greater_than: 0, less_than: 1000 }
+  validates :location, presence: true
   has_many :attendances
   belongs_to :admin, class_name: 'User'
   has_many :attendees, through: :attendances, source: :user
 
   def confirm_send
     AdminMailer.confirm_email(self).deliver_now
+  end
+
+  def start_date_cannot_be_in_the_past
+    errors.add(:start_date, "can't be in the past") if
+      !start_date.blank? && start_date < Date.today
+  end
+
+  def not_multiple_of_five?
+    errors.add(:duration, 'must be a multiple of 5') if
+      !duration.blank? && duration % 5 != 0
   end
 end
