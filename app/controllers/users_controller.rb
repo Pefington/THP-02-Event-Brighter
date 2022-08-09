05e279265647f1af597a4303c497a3002966c9ca
Samsou1 <sam.moreau0@gmail.com>
2022-08-09 16:26:09 +0200

    user show and user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,26 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    # @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def update
-
+    @user = User.find(params[:id])
+    if @user.update(
+      email: params[:email],
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      description: params[:description]
+    )
+      flash.now[:success] = 'User successfully updated!'
+      redirect_to user_path(@user.id)
+    else
+      @user.errors.full_messages.each { |m| flash.now[:alert] = m }
+      render :edit
+    end
   end
 end
