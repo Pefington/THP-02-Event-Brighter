c1c6623fb1387bde80ff2e7fdc4eec11f797fd37
Samsou1 <sam.moreau0@gmail.com>
2022-08-10 15:36:21 +0200

    view index attendance

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,20 +1,20 @@
 class AttendancesController < ApplicationController
 
+  def index
+    @event = Event.find(params[:id])
+    @attendees = Attendance.where(event:@event)
+  end
+
   def new
     @event = Event.find(params[:id])
   end
 
   def create
-    # if payment = true
+    # if payment == true
     #   redirect_to event_path(params[:id])
     # else
     #   render 
     # end
-  end
-
-  def index
-    @event = Event.where(id:params[:id])
-    @attendance = Attendance.where(event:@event)
   end
 
 end
