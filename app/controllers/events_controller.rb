134fff97c50ac02d8279dd62be7714982c89a16d
Samsou1 <sam.moreau0@gmail.com>
2022-08-10 16:13:06 +0200

    many changes in event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,27 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def edit
+    @event = Event.find(params[:id])
+    if @event.update(
+      description: params[:description],
+      start_date: params[:start_date],
+      duration: params[:duration],
+      location: params[:location],
+      price: params[:price]
+    )
+      flash[:success] = 'You have successfully created a new event'
+      redirect_to event_path(@event.id)
+    else
+      @event.errors.full_messages.each { |m| flash.now[:alert] = m }
+      render edit_event_path(@event.id)
+    end
+  end
+
+  def update
+
+  end
+
   def show
     @event = Event.find(params[:id])
     @count = Attendance.where(event: @event).count
@@ -31,5 +52,12 @@ class EventsController < ApplicationController
       @event.errors.full_messages.each { |m| flash.now[:alert] = m }
       render new_event_path
     end
+  end
+
+  def destroy
+    @event = Event.find_by_id(params[:id])
+    @event.destroy
+    flash[:success] = 'The gossip was destroyed'
+    redirect_to event_index_path
   end
 end
