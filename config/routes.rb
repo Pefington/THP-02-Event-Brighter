60a9d4b4d43674db487e8e33e280b1b96acc5cc8
Pierre-François Salmand <71193709+Pefington@users.noreply.github.com>
2022-08-17 23:42:38 +0700

    Implement partials (#10)
    * footer
    
    * fash footer navbar
    
    * mise en forme views
    
    * event show
    
    * Integrate into asset pipeline
    
    * profile show
    
    * WIP on layout
    
    * Finish CSS on _footer
    
    * Finish _signup integration
    
    * Add live reload
    
    * Finish _signup erb
    
    * Finish intgrating _signup
    
    * Fix bs theme
    
    Co-authored-by: Samsou1 <sam.moreau0@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   get 'profile', action: :show, controller: 'users'
   post 'profile', action: :update, controller: 'users'
   get 'profile/edit', action: :edit, controller: 'users'
-  namespace :admin do
+  # namespace :admin do
     resources :users, only: %i[index edit show update destroy]
     resources :events, only: %i[index edit show update destroy]
-  end
+  # end
   resources :events do
     resources :attendances, only: %i[create new index]
   end
